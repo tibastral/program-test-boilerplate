daf0cd24a13957545d48e5329a23c6f5a0acb828
Thibaut Assus <thibaut@milesrock.com>
2025-04-03 14:01:41 +0200

    More consistant

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,10 +3,15 @@ module Tests exposing (..)
 import Effect.Test as TF
 import HelloWorldTest
 import Test exposing (..)
-
+import Types exposing (BackendModel, BackendMsg, FrontendModel, FrontendMsg, ToBackend, ToFrontend)
 
 appTests : Test
 appTests =
     describe "App tests" 
         [ describe "Hello World" (List.map TF.toTest HelloWorldTest.tests)
         ]
+
+
+tests : List (TF.EndToEndTest ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel)
+tests =
+     HelloWorldTest.tests 
